9969c9406486cc5bce35179e6f7e79f6208b5cd6
Babouche <clnabergel@hotmail.fr>
2024-05-05 22:26:34 -0400

    Inital setup for tools (#1)
    * Initial
    
    * Create PythonFormatter.yml
    
    * Format code and readme
    
    * Update python version for CI
    
    * Switching for 3.8
    
    * Add folder checking
    
    * Update README.md
    
    * Update README.md
    
    * Refactor all the code in functions
    
    * Add command line explaination
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # BuggyBot
 Simply a customs agent
+
+# Directories
+
+## Bot
+This directory contains all the source code.
+
+## Tools
+This directory contains tools to help development. Add all python files that will help you to dev easier.
+
+:warning: This directory should not be called from the `Bot` directory.
+
+:arrow_right: You can use constants from the Bot directory if needed.
+
+### Commands
+To upload a new list of students provided by the department, run :
+```commandline
+python ./tools/update_students_list.py FILENAME.xlsx
+```
+
+# Contribution
+- Download Python 3.8.10 via https://www.python.org/downloads/release/python-3810/
+- Download MongoDB Community Server via https://fastdl.mongodb.org/windows/mongodb-windows-x86_64-7.0.9-signed.msi
+- Download Mongo MongoDB Compass via https://downloads.mongodb.com/compass/mongodb-compass-1.43.0-win32-x64.exe
+
+:warning: Create a `.env` in the project root in order to interact with mongoDB. See section `.env setup` for more.
+
+:warning: Do not forget to sync your requirements.txt. See section below.
+
+## Adding package to project
+Sync your requirements.txt with PyCharm via https://www.jetbrains.com/help/pycharm/managing-dependencies.html
+
+## .env configuration
+- At the root of the project `./BuggyBot` create a file and name it `.env`.
+- Add the following lines:
+```md
+MONGODB_LOCALHOST_SERVER_CONNECTION_STRING = "YOUR MONGODB CONNECTION STRING"
+MONGODB_DB_NAME = "YOUR MONGODB NAME"
+```
+
+## Commands
+To fix linter et format error run theses two commands
+```commandline
+ruff check . --fix
+```
+```commandline
+black .
+```
---
+++ tools/update_students_list.py
@@ -0,0 +1,110 @@
+import argparse
+import pandas as pd
+from typing import Dict, Optional, List
+from dotenv import dotenv_values
+from pymongo import MongoClient
+from constants import StudentListKey, UniProgram
+
+ARGUMENT_FILENAME_KEY = "csv_filename"
+STUDENTS_LIST_COLUMNS_TO_KEEP: List[str] = [
+    StudentListKey.NI,
+    StudentListKey.PROGRAM_CODE,
+    StudentListKey.LASTNAME,
+    StudentListKey.FIRSTNAME,
+    StudentListKey.NEW,
+]
+
+STUDENT_ALLOWED_PROGRAM = [UniProgram.IFT, UniProgram.GLO, UniProgram.CERTIFICATE]
+STUDENT_NOUVEAU_COLUMN_MAPPING = {"Oui": True, "Non": False}
+
+MISSING_DISCORD_USER_ID = -1
+
+
+def read_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Read CSV file and keep certain columns"
+    )
+    parser.add_argument(ARGUMENT_FILENAME_KEY, type=str, help="Path to the CSV file")
+    return parser.parse_args()
+
+
+def read_configurations() -> Dict[str, Optional[str]]:
+    try:
+        return dotenv_values("../.env")
+    except FileNotFoundError as e:
+        print(f".env file is not found. {e}")
+        exit(-1)
+
+
+def connect_to_mongo_db(connection_url: str) -> MongoClient:
+    try:
+        return MongoClient(connection_url)
+    except ConnectionError as e:
+        print(f"Unable to connect to the MongoDB. {e}")
+        exit(-1)
+
+
+def read_students_csv(file_path: str) -> pd.DataFrame:
+    return pd.read_excel(file_path, usecols=STUDENTS_LIST_COLUMNS_TO_KEEP)
+
+
+def filter_students_by_program(students_df: pd.DataFrame) -> pd.DataFrame:
+    return students_df[
+        students_df[StudentListKey.PROGRAM_CODE].isin(STUDENT_ALLOWED_PROGRAM)
+    ].copy()
+
+
+def map_nouveau_column(students_df: pd.DataFrame) -> pd.DataFrame:
+    students_df.loc[:, StudentListKey.NEW] = students_df.loc[
+        :, StudentListKey.NEW
+    ].replace(STUDENT_NOUVEAU_COLUMN_MAPPING)
+    return students_df
+
+
+def get_nis_from_collection(collection) -> set:
+    return set(
+        col[StudentListKey.NI] for col in collection.find({}, {StudentListKey.NI: 1})
+    )
+
+
+def get_students_to_insert(students_df: pd.DataFrame, existing_nis: set) -> List:
+    return [
+        row.to_dict()
+        for _, row in students_df.iterrows()
+        if row[StudentListKey.NI] not in existing_nis
+    ]
+
+
+def insert_students_into_collection(collection, students_to_insert):
+    if len(students_to_insert):
+        collection.insert_many(students_to_insert)
+
+
+def main():
+    arguments = read_arguments()
+    configurations = read_configurations()
+
+    client = connect_to_mongo_db(
+        configurations["MONGODB_LOCALHOST_SERVER_CONNECTION_STRING"]
+    )
+    database = client[configurations["MONGODB_DB_NAME"]]
+    students_collection = database["students"]
+
+    students_df = read_students_csv(arguments.csv_filename)
+    filtered_students_df = filter_students_by_program(students_df)
+    filtered_students_df = map_nouveau_column(filtered_students_df)
+
+    existing_nis = get_nis_from_collection(students_collection)
+    students_to_insert = get_students_to_insert(filtered_students_df, existing_nis)
+
+    for student in students_to_insert:
+        student[StudentListKey.DISCORD_USER_ID] = MISSING_DISCORD_USER_ID
+
+    insert_students_into_collection(students_collection, students_to_insert)
+
+    print(f"{len(students_to_insert)} new students inserted")
+    print("Students table has been updated!")
+
+
+if __name__ == "__main__":
+    main()
