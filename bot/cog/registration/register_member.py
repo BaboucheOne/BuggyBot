7ec9d11a48f7506dbf81b17d5d63ee678d83ce16
Mathis Rosa-Wilson <146891332+EctobiusRexV@users.noreply.github.com>
2024-05-13 22:06:35 -0400

    Message de bienvenue dans un fichier de constante

--- bot/cog/registration/register_member.py
+++ bot/cog/registration/register_member.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from tools.constants import Messages
 
 
 class RegisterMemberCog(commands.Cog):
@@ -8,7 +9,4 @@ class RegisterMemberCog(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member):
         await member.create_dm()
-        await member.dm_channel.send(f"Bienvenue {member.name} dans le serveur Discord de l'ASETIN (Association des étudiantes et étudiants en informatique)! \n"
-                                     "Afin de confirmer que tu es bel et bien un(e) étudiant(e) inscrit(e) à l'association, entre ton numéro d'identification personnel (NI).\n"
-                                     "Au plaisir de te rencontrer!\n"
-                                     "ps: Si tu ne sais pas où trouver ton NI, tu peux le consulter sur MonPortail ou encore sur ta carte étudiante.")
+        await member.dm_channel.send(Messages.WELCOME)
--- tools/constants.py
+++ tools/constants.py
@@ -17,3 +17,9 @@ class UniProgram:
     IIG: str = "B-IIG"
     CERTIFICATE: str = "C-IFT"
 
+
+class Messages:
+    WELCOME: str = "Bienvenue dans le serveur Discord de l'ASETIN (Association des étudiantes et étudiants en informatique)! \n"\
+                  "Afin de confirmer que tu es bel et bien un(e) étudiant(e) inscrit(e) à l'association, entre ton numéro d'identification personnel (NI).\n"\
+                  "Au plaisir de te rencontrer!\n"\
+                  "ps: Si tu ne sais pas où trouver ton NI, tu peux le consulter sur MonPortail ou encore sur ta carte étudiante."
