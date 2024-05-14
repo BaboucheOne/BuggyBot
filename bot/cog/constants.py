041ae22b44a98e08847fe78efa048e9f9db35d6a
Babouche <clnabergel@hotmail.fr>
2024-05-14 00:10:37 -0400

    Move constants in correct folder

---
+++ bot/cog/constants.py
@@ -0,0 +1,14 @@
+class ReplyMessages:
+    WELCOME: str = (
+        "Bienvenue dans le serveur Discord de l'ASETIN (Association des étudiantes et étudiants en informatique)! \n"
+        "Afin de confirmer que tu es bel et bien un(e) étudiant(e) inscrit(e) à l'association, entre ton numéro d'identification personnel (NI).\n"
+        "Au plaisir de te rencontrer!\n"
+        "ps: Si tu ne sais pas où trouver ton NI, tu peux le consulter sur MonPortail ou encore sur ta carte étudiante."
+    )
+
+    ALREADY_REGISTERED: str = (
+        "Your are already registered.\nIf you haven't registered yet, please contact an admin."
+    )
+    UNABLE_TO_REGISTER: str = (
+        "Unable to registered you.\nCheck your NI. If your NI is the good one, please contact an admin."
+    )
