9a17fb6a9699cf4c4c45afea9434815cddaa20f1
Babouche <clnabergel@hotmail.fr>
2024-06-10 10:14:04 -0400

    Add context configuration

--- tools/update_students_list.py
+++ tools/update_students_list.py
@@ -6,8 +6,10 @@ from pymongo import MongoClient
 
 from bot.config.constants import ConfigurationFilename
 from bot.config.dotenv_configuration import DotEnvConfiguration
+from bot.domain.student.attribut.discord_user_id import DiscordUserId
 from bot.infra.constants import StudentMongoDbKey
 from constants import StudentListKey, UniProgram
+from tools.common import get_configuration, add_configuration_argument
 
 ARGUMENT_FILENAME_KEY = "csv_filename"
 STUDENTS_LIST_COLUMNS_TO_KEEP: List[str] = [
@@ -34,14 +36,14 @@ STUDENTS_LIST_RENAMING_MAPPING = {
     StudentListKey.NEW: StudentMongoDbKey.NEW_ADMITTED,
 }
 
-MISSING_DISCORD_USER_ID = -1
-
 
 def read_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         description="Read CSV file and keep certain columns"
     )
     parser.add_argument(ARGUMENT_FILENAME_KEY, type=str, help="Path to the CSV file")
+    add_configuration_argument(parser)
+
     return parser.parse_args()
 
 
@@ -115,7 +117,7 @@ def get_dot_env_filepath():
 
 def main():
     arguments = read_arguments()
-    configuration = read_configurations(ConfigurationFilename.DEVELOPMENT)
+    configuration: DotEnvConfiguration = get_configuration(arguments)
 
     client = connect_to_mongo_db(configuration.mongodb_connection_string)
     database = client[configuration.mongodb_database_name]
@@ -130,7 +132,7 @@ def main():
     students_to_insert = get_students_to_insert(filtered_students_df, existing_nis)
 
     for student in students_to_insert:
-        student[StudentMongoDbKey.DISCORD_USER_ID] = MISSING_DISCORD_USER_ID
+        student[StudentMongoDbKey.DISCORD_USER_ID] = DiscordUserId.INVALID_DISCORD_ID
 
     insert_students_into_collection(students_collection, students_to_insert)
 
