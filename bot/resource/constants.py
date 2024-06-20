f59bb4a7a523643ef08777cf3630dc214ebf5587
Nathaniel Girard <nathaniel.girard1@gmail.com>
2024-06-19 20:06:05 -0400

    Changed from 1st person singular to 3rd plural

--- bot/resource/constants.py
+++ bot/resource/constants.py
@@ -1,16 +1,16 @@
 class ReplyMessage:
     WELCOME: str = (
         "Bienvenue dans le serveur Discord de l'ASETIN.\n"
-        "Afin de confirmer que tu es bel et bien un(e) étudiant(e) inscrit(e) à l'association, entre ton numéro d'identification personnel (NI) via la commande !register [NI].\n"
-        "Au plaisir de te rencontrer!\n"
-        "PS: Si tu ne sais pas où trouver ton NI, clique [ici](https://monportail.ulaval.ca/portail/carte-identite/apercus-carte).\n"
-        "En tout temps, écris !help pour connaître les options qui s'offrent à toi."
+        "Afin de confirmer que vous êtes bel et bien un(e) étudiant(e) inscrit(e) à l'association, entrez votre numéro d'identification personnel (NI) via la commande !register [NI].\n"
+        "Au plaisir de vous rencontrer!\n"
+        "PS: Si vous ne savez pas où trouver votre NI, cliquez [ici](https://monportail.ulaval.ca/portail/carte-identite/apercus-carte).\n"
+        "En tout temps, écrivez !help pour connaître les options qui s'offrent à vous."
     )
 
     UNSUCCESSFUL_GENERIC: str = "Oh oh... quelque chose s'est mal passé."
 
     ALREADY_REGISTERED: str = (
-        "Vous êtes déjà enregistré. Si vous ne vous êtes pas encore enregistré, veuillez contacter un administrateur."
+        "Vous êtes déjà enregistré(e). Si vous ne vous êtes pas encore enregistré, veuillez contacter un administrateur."
     )
 
     UNABLE_TO_REGISTER: str = (
@@ -25,12 +25,12 @@ class ReplyMessage:
         "Étudiant ajouté ! Il peut maintenant s'inscrire lui-même en m'envoyant un message privé."
     )
 
-    SUCCESSFUL_UNREGISTER: str = "L'utilisateur a été désenregistré avec succès."
+    SUCCESSFUL_UNREGISTER: str = "L'utilisateur a été supprimé avec succès."
 
     STUDENT_ALREADY_EXISTS: str = "Cet étudiant existe déjà."
 
     MISSING_ARGUMENTS_IN_COMMAND: str = "Arguments manquants dans la commande."
 
     NOTIFY_UNREGISTER: str = (
-        "Vous avez été désenregistré du serveur ASETIN. Contactez un administrateur pour plus d'informations."
+        "Vous avez été supprimé du serveur ASETIN. Contactez un administrateur pour plus d'informations."
     )
