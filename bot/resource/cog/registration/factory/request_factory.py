f3a44b691985b5295489415614ad417025714c98
Babouche <clnabergel@hotmail.fr>
2024-07-23 11:26:52 -0400

    Fix composed name for add_student command (#108)

--- bot/resource/cog/registration/factory/request_factory.py
+++ bot/resource/cog/registration/factory/request_factory.py
@@ -1,5 +1,5 @@
 from abc import ABC
-from typing import Tuple
+from typing import Tuple, List
 
 from bot.resource.exception.missing_arguments_exception import MissingArgumentsException
 
@@ -8,12 +8,17 @@ class RequestFactory(ABC):
     def __init__(self, number_of_arguments):
         self._number_of_arguments = number_of_arguments
 
-    def _get_arguments(self, content: str) -> Tuple[any, ...] | any:
+    def __strip_arguments(self, arguments: List[str]) -> List[str]:
+        return [argument.strip() for argument in arguments]
+
+    def _get_arguments(self, content: str) -> Tuple[any, ...] or any:
         content = content.rstrip()
-        arguments = content.split(" ", self._number_of_arguments - 1)
+        arguments = content.split(",", self._number_of_arguments - 1)
 
         if len(arguments) != self._number_of_arguments:
             raise MissingArgumentsException(self._number_of_arguments)
+
+        arguments = self.__strip_arguments(arguments)
 
         if len(arguments) == 1:
             return arguments[0]
