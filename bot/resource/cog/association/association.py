77729c0d12bdd9be1ccfa255047ff96bb56b2408
Babouche <clnabergel@hotmail.fr>
2024-07-24 09:19:21 -0400

    Add decorator to association cog

--- bot/resource/cog/association/association.py
+++ bot/resource/cog/association/association.py
@@ -1,4 +1,3 @@
-from discord import Message
 from discord.ext import commands
 from discord.ext.commands import Context
 
@@ -8,6 +7,7 @@ from bot.resource.constants import ReplyMessage
 from bot.config.logger.logger import Logger
 from bot.config.service_locator import ServiceLocator
 from bot.domain.discord_client.discord_client import DiscordClient
+from bot.resource.decorator.prohibit_self_message import prohibit_self_message
 
 
 class AssociationCog(commands.Cog, name="Association"):
@@ -16,15 +16,13 @@ class AssociationCog(commands.Cog, name="Association"):
         self.__bot = ServiceLocator.get_dependency(DiscordClient)
         self.__logger: Logger = ServiceLocator.get_dependency(Logger)
 
-    def __is_self(self, message: Message) -> bool:
-        return message.author == self.__bot.user
-
     @commands.command(
         name="asetin",
         help="Informations sur l'ASETIN.",
         brief="Informations sur l'ASETIN.",
     )
     @commands.dm_only()
+    @prohibit_self_message()
     async def asetin(self, context: Context):
         self.__logger.info(
             f"Exécution de la commande par {context.message.author}.",
@@ -32,8 +30,6 @@ class AssociationCog(commands.Cog, name="Association"):
         )
 
         message = context.message
-        if self.__is_self(message):
-            return
 
         try:
             await message.channel.send(embed=AsetinEmbed().embed)
@@ -54,6 +50,7 @@ class AssociationCog(commands.Cog, name="Association"):
         brief="Informations sur l'AEGLO.",
     )
     @commands.dm_only()
+    @prohibit_self_message()
     async def aeglo(self, context: Context):
         self.__logger.info(
             f"Exécution de la commande exécutée par {context.message.author}",
@@ -61,8 +58,6 @@ class AssociationCog(commands.Cog, name="Association"):
         )
 
         message = context.message
-        if self.__is_self(message):
-            return
 
         try:
             await message.channel.send(embed=AegloEmbed().embed)
