be20f26278553b3a27a45a9f14caff93bcaafb7a
Babouche <clnabergel@hotmail.fr>
2024-07-25 23:56:16 -0400

    Refactor decorators & attributs

--- bot/resource/decorator/prohibit_self_message.py
+++ bot/resource/decorator/prohibit_self_message.py
@@ -7,6 +7,6 @@ from bot.domain.discord_client.discord_client import DiscordClient
 def prohibit_self_message():
     def predicate(ctx):
         discord_client: DiscordClient = ServiceLocator.get_dependency(DiscordClient)
-        return ctx.message.author != discord_client.user
+        return ctx.message.author.id != discord_client.user.id
 
     return commands.check(predicate)
--- bot/resource/decorator/role_check.py
+++ bot/resource/decorator/role_check.py
@@ -1,5 +1,5 @@
-import discord
 from discord.ext import commands
+from discord.utils import get
 
 from bot.config.service_locator import ServiceLocator
 from bot.domain.discord_client.discord_client import DiscordClient
@@ -7,16 +7,12 @@ from bot.domain.discord_client.discord_client import DiscordClient
 
 def role_check(*role_names):
     def predicate(ctx):
-        user = ctx.message.author
-        discord_client: DiscordClient = ServiceLocator.get_dependency(DiscordClient)
-        member = discord_client.server.get_member(user.id)
-        if member is None:
+        discord_client = ServiceLocator.get_dependency(DiscordClient)
+        member = discord_client.server.get_member(ctx.message.author.id)
+
+        if not member:
             return False
 
-        for role_name in role_names:
-            role = discord.utils.get(member.roles, name=role_name)
-            if role is not None:
-                return True
-        return False
+        return any(get(member.roles, name=role_name) for role_name in role_names)
 
     return commands.check(predicate)
