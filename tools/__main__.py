d702b9471abe9f451fd1e30da4bc491d10a4e321
Babouche <clnabergel@hotmail.fr>
2024-08-20 13:40:43 -0400

    Add tool

--- tools/__main__.py
+++ tools/__main__.py
@@ -9,11 +9,13 @@ sys.path.append(bot_directory_path)
 import migrate_discord_students  # noqa: E402
 import update_students_list  # noqa: E402
 import renew_students_list  # noqa: E402
+import give_integration_role  # noqa: E402
 
 
 TOOL_NAME_MIGRATE_STUDENT: str = "migrate_students"
 TOOL_NAME_UPDATE_STUDENTS_LIST: str = "update_students_list"
 TOOL_NAME_RENEW_STUDENTS_LIST: str = "renew_students_list"
+TOOL_NAME_GIVE_INTEGRATION_ROLE: str = "give_integration_role"
 
 
 def read_arguments() -> argparse.Namespace:
@@ -27,6 +29,7 @@ def read_arguments() -> argparse.Namespace:
             TOOL_NAME_MIGRATE_STUDENT,
             TOOL_NAME_UPDATE_STUDENTS_LIST,
             TOOL_NAME_RENEW_STUDENTS_LIST,
+            TOOL_NAME_GIVE_INTEGRATION_ROLE,
         ],
     )
 
@@ -40,3 +43,5 @@ elif args.command == TOOL_NAME_UPDATE_STUDENTS_LIST:
     asyncio.run(update_students_list.main(arguments=sys.argv[2:]))
 elif args.command == TOOL_NAME_RENEW_STUDENTS_LIST:
     asyncio.run(renew_students_list.main(arguments=sys.argv[2:]))
+elif args.command == TOOL_NAME_GIVE_INTEGRATION_ROLE:
+    asyncio.run(give_integration_role.main(arguments=sys.argv[2:]))
