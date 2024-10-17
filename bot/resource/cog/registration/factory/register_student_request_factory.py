096cd922b879e1ede9a013adace4bf0e6a69b266
Babouche <clnabergel@hotmail.fr>
2024-10-17 14:24:20 -0400

    Switch for an utility class

--- bot/resource/cog/registration/factory/register_student_request_factory.py
+++ bot/resource/cog/registration/factory/register_student_request_factory.py
@@ -1,19 +1,16 @@
-from bot.resource.chain_of_responsibility.responsibility_handler import (
-    ResponsibilityHandler,
-)
 from bot.resource.cog.registration.factory.request_factory import RequestFactory
 from bot.resource.cog.registration.request.register_student_request import (
     RegisterStudentRequest,
 )
+from bot.resource.utility.sanitizer_utility import SanitizerUtility
 
 
 class RegisterStudentRequestFactory(RequestFactory):
     REQUIRED_ARGUMENTS = 1
 
-    def __init__(self, ni_sanitizer: ResponsibilityHandler):
+    def __init__(self):
         super().__init__(self.REQUIRED_ARGUMENTS)
-        self.__ni_sanitizer = ni_sanitizer
 
     def create(self, content: str, author_id: int) -> RegisterStudentRequest:
         ni = self._get_arguments(content)
-        return RegisterStudentRequest(self.__ni_sanitizer.handle(ni), author_id)
+        return RegisterStudentRequest(SanitizerUtility.sanitize_ni(ni), author_id)
