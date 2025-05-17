dfc461f544a9ca0ed111f724556826a0d6519131
Babouche <clnabergel@hotmail.fr>
2025-05-16 23:41:10 -0400

    add tests

---
+++ tests/application/validators/test_name_validator.py
@@ -0,0 +1,49 @@
+import pytest
+
+from bot.application.student.validators.name_validator import NameValidator
+
+NAME: str = "Clemou Delta"
+COMPOSED_NAME: str = "Clemou-Delta"
+TOO_LONG_NAME: str = "Jean-Pierre Alexandre-François de la Fontaine"
+EMPTY_NAME: str = ""
+CAMEL_CASE_NAME: str = "clemou delta"
+CAMEL_CASE_COMPOSED_NAME: str = "clemou-delta"
+UPPER_CASE_NAME: str = "CLEMOU DELTA"
+UPPER_CASE_COMPOSED_NAME: str = "CLEMOU-DELTA"
+
+
+@pytest.fixture
+def name_validator() -> NameValidator:
+    return NameValidator()
+
+
+def test__when_using_valid_name__then_return_true(name_validator):
+    assert name_validator.validate(NAME)
+
+
+def test_when_using_composed_name__then_return_true(name_validator):
+    assert name_validator.validate(COMPOSED_NAME)
+
+
+def test__when_using_too_long_name__then_return_false(name_validator):
+    assert not name_validator.validate(TOO_LONG_NAME)
+
+
+def test__when_using_empty_name__then_return_false(name_validator):
+    assert not name_validator.validate(EMPTY_NAME)
+
+
+def test__when_using_camel_case_name__then_return_false(name_validator):
+    assert not name_validator.validate(CAMEL_CASE_NAME)
+
+
+def test__when_using_camel_composed_name__then_return_false(name_validator):
+    assert not name_validator.validate(CAMEL_CASE_COMPOSED_NAME)
+
+
+def test__when_using_upper_case_name__then_return_false(name_validator):
+    assert not name_validator.validate(UPPER_CASE_NAME)
+
+
+def test__when_using_upper_composed_name__then_return_false(name_validator):
+    assert not name_validator.validate(UPPER_CASE_COMPOSED_NAME)
--- tests/application/validators/test_ni_validator.py
+++ tests/application/validators/test_ni_validator.py
@@ -2,10 +2,28 @@ import pytest
 
 from bot.application.student.validators.ni_validator import NIValidator
 
+NI: str = "111222333"
+INVALID_DIGIT_LEN_NI: str = "11122233"
+NON_DIGIT_NI: str = "abcdefghi"
+LETTER_AND_DIGIT_NI: str = "123abc456"
+
 
 @pytest.fixture
-def ni_validator():
+def ni_validator() -> NIValidator:
     return NIValidator()
 
-def test_ni_true(ni_validator):
-    assert ni_validator.validate("111228454")
+
+def test__when_validate_valid_ni__should_return_true(ni_validator):
+    assert ni_validator.validate(NI)
+
+
+def test__when_validate_invalid_length_ni__should_return_false(ni_validator):
+    assert not ni_validator.validate(INVALID_DIGIT_LEN_NI)
+
+
+def test__when_validate_non_digit_ni_should__return_false(ni_validator):
+    assert not ni_validator.validate(NON_DIGIT_NI)
+
+
+def test__when_validate_letter_and_digit_ni__should_return_false(ni_validator):
+    assert not ni_validator.validate(LETTER_AND_DIGIT_NI)
