41920696022ae71d592efc4da8a6bf31b18ddcdf
Babouche <clnabergel@hotmail.fr>
2025-05-17 09:56:21 -0400

    Add validator tests (#174)

--- bot/application/student/validators/discord_id_validator.py
+++ bot/application/student/validators/discord_id_validator.py
@@ -1,9 +1,14 @@
 class DiscordIdValidator:
 
+    OLD_DISCORD_ID_DIGITS_COUNT = 17
     DISCORD_ID_DIGITS_COUNT = 18
 
     def __init__(self):
         pass
 
     def validate(self, discord_id: int) -> bool:
-        return len(str(discord_id)) == self.DISCORD_ID_DIGITS_COUNT
+        id_length = len(str(discord_id))
+        return (
+            id_length == self.OLD_DISCORD_ID_DIGITS_COUNT
+            or id_length == self.DISCORD_ID_DIGITS_COUNT
+        )
---
+++ tests/application/validators/test_discord_id_validator.py
@@ -0,0 +1,24 @@
+import pytest
+
+from bot.application.student.validators.discord_id_validator import DiscordIdValidator
+
+DISCORD_ID: int = 123456789012749572
+OLD_DISCORD_ID: int = 1234567891023567
+INVALID_LENGTH_DISCORD_ID: int = 2133
+
+
+@pytest.fixture
+def discord_id_validator() -> DiscordIdValidator:
+    return DiscordIdValidator()
+
+
+def test__when_using_old_discord_id__then_return_true(discord_id_validator):
+    assert not discord_id_validator.validate(OLD_DISCORD_ID)
+
+
+def test__when_using_discord_id__then_return_true(discord_id_validator):
+    assert discord_id_validator.validate(DISCORD_ID)
+
+
+def test__when_using_invalid_length_discord_id__then_return_false(discord_id_validator):
+    assert not discord_id_validator.validate(INVALID_LENGTH_DISCORD_ID)
